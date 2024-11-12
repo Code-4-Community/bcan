06b9afba38e6826b9430d6b4bb542ac1cb2dcffd
jarenadams21 <94200103+jarenadams21@users.noreply.github.com>
2024-11-12 18:02:26 -0500

    Updates (#26)
    * simplified PR
    
    * notifications scaffold
    
    * overwrite
    
    * login code too
    
    ---------
    
    Co-authored-by: jaren

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,11 @@ import AWS from 'aws-sdk';
 
 /* ! */
 async function bootstrap() {
-    AWS.config.update({
-        region: process.env.AWS_REGION
-      });
+  AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.OPEN_HATCH,
+    secretAccessKey: process.env.CLOSED_HATCH
+  });  
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3001);
