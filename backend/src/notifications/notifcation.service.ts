a6557b2d21f7bf09507227c1d9da53e9e4883059
aditya161216 <59684507+aditya161216@users.noreply.github.com>
2024-12-12 15:14:51 -0500

    Fixed notification module

--- backend/src/notifications/notifcation.service.ts
+++ backend/src/notifications/notifcation.service.ts
@@ -8,6 +8,7 @@ import { Notification } from './notification.model'; // Adjust the path as neede
 
 AWS.config.update({ region: 'us-east-1' });  
 // AWS.config.update({ region: 'us-east-2' });   
+// const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 
@@ -33,31 +34,73 @@ export class NotificationService {
  }
 
 
- // function to find notifications by notification id
- async getNotificationByUserId(userId: string): Promise<Notification> {
+ // function to find notifications by user id (sorted by most recent notifications first)
+  async getNotificationByUserId(userId: string): Promise<Notification[]> {
 
+    console.log("USER ID", userId)
 
-   const params = {
-       TableName: process.env.DYNAMODB_NOTIFICATION_TABLE_NAME || 'TABLE_FAILURE',
-       Key: {
-           userId : userId
-       },
-   };
+    // KeyConditionExpression specifies the query condition
+    // ExpressionAttributeValues specifies the actual value of the key
+    // IndexName specifies our Global Secondary Index, which was created in the BCANNotifs table to 
+    // allow for querying by userId, as it is not a primary/partition key
+    const params = {
+      TableName: process.env.DYNAMODB_NOTIFICATION_TABLE_NAME || 'TABLE_FAILURE',
+      IndexName: 'userId-alertTime-index',
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId,
+      },
+      ScanIndexForward: false // sort in descending order
+    };
 
 
    try {
-       const data = await dynamodb.get(params).promise();
+       const data = await dynamodb.query(params).promise();
 
 
-       if (!data.Item) {
-           throw new Error('No notification with user id ' + userId + ' found.');
+       if (!data.Items) {
+         throw new Error('No notifications with user id ' + userId + ' found.');
        }
 
 
-       return data.Item as Notification;
+       return data.Items as Notification[];
    } catch (error) {
        console.log(error)
-       throw new Error('Failed to retrieve notification.');
+       throw new Error('Failed to retrieve notifications.');
    }
 }
+
+
+  // function to find notifications by notification id
+  async getNotificationByNotificationId(notificationId: string): Promise<Notification[]> {
+
+    console.log("NOTIF ID", notificationId)
+
+    // key condition expression specifies the query condition
+    // expression attribute values specifies the actual value of the key
+    const params = {
+      TableName: process.env.DYNAMODB_NOTIFICATION_TABLE_NAME || 'TABLE_FAILURE',
+      KeyConditionExpression: 'notificationId = :notificationId',
+      ExpressionAttributeValues: {
+        ':notificationId': notificationId,
+      },
+    };
+
+
+    try {
+      const data = await dynamodb.query(params).promise();
+
+
+      if (!data.Items) {
+        throw new Error('No notifications with notification id ' + notificationId + ' found.');
+      }
+
+
+      return data.Items as Notification[];
+    } catch (error) {
+      console.log(error)
+      throw new Error('Failed to retrieve notification.');
+    }
+  }
+
 }
