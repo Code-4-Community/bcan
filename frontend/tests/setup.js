2cdf15288db910f7facea18df2b3be362cb8b503
Jaren Adams <jarenadams@Jarens-MacBook-Air.local>
2025-01-28 19:49:35 -0500

    pre-merge fixes for node 20

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -2,13 +2,6 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
 
-// Polyfill for crypto.getRandomValues in JSDOM
-import { webcrypto } from 'crypto';
-
-if (!global.crypto) {
-  global.crypto = webcrypto;
-}
-
 expect.extend(matchers);
 
 afterEach(() => {
