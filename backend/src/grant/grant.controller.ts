18669e0521e8450d92ef94de27302d491bf0a48d
prooflesben <prooflesben@gmail.com>
2025-03-19 10:26:57 -0400

    Basiclaly did the gratn history but can't test

--- backend/src/grant/grant.controller.ts
+++ backend/src/grant/grant.controller.ts
@@ -16,10 +16,10 @@ export class GrantController {
         return await this.grantService.getGrantById(parseInt(GrantId, 10));
     }
 
-    @Get('/history:id')
-    async getGrantHistory(@Param('id') GrantId: string) {
-        return await this.grantService.getGrantHistory(parseInt(GrantId, 10));
-    }
+    // @Get('/history:id')
+    // async getGrantHistory(@Param('id') GrantId: string) {
+    //     return await this.grantService.getGrantHistory(parseInt(GrantId, 10));
+    // }
 
     @Put('archive')
     async archive(
