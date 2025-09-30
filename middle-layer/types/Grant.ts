1535dd40f4b94935b0d9f68bf6e277626cd3054c
lyannne <xu.ly@northeastern.edu>
2025-09-30 16:12:37 -0400

    updated grant entity in middleware, made report deadlines multi-input in frontend

--- middle-layer/types/Grant.ts
+++ middle-layer/types/Grant.ts
@@ -1,6 +1,7 @@
 import { TDateISO } from "../../backend/src/utils/date";
 import Attachment from "./Attachment";
 import { Status } from "./Status";
+import POC from "./POC";
 
 // model for Grant objects
 export interface Grant {
@@ -9,14 +10,15 @@ export interface Grant {
     does_bcan_qualify: boolean;
     status: Status;
     amount: number;
+    grant_start_date: TDateISO; // when the grant was started
     application_deadline: TDateISO; // when was grant submission due
-    report_deadline: TDateISO;       // when is next report due
-    notification_date: TDateISO; // date to hear back
+    report_deadlines: TDateISO[];       // multiple report dates
     description: string;
-    application_requirements: string;
-    additional_notes: string;
     timeline: number; // Need to specify
     estimated_completion_time: number,
-    grantmaker_poc: string[]; // array of emails
+    grantmaker_poc: POC; // person of contact at organization giving the grant
+    // bcan_poc may need to be changed later to be a validated account
+    bcan_poc: POC; // person of contact at BCAN
     attachments: Attachment[];
+    restricted_or_unrestricted: string; // "restricted" or "unrestricted"
 }
