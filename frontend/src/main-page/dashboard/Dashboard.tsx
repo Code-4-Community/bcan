c9e28473d9dddacaff06c60e1083e6aeacc6878c
Jane Kamata <janekamata8@gmail.com>
2025-10-17 00:30:02 -0400

    Created CSV export button

--- frontend/src/main-page/dashboard/Dashboard.tsx
+++ frontend/src/main-page/dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
+import CsvExportButton from "./CsvExportButton";
 import "./styles/Dashboard.css";
 
 function Dashboard() {
 
   return (
     <div className="dashboard-page">
+      <CsvExportButton />
     </div>
   );
 }
