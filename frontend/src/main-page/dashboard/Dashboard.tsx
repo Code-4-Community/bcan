3909e2cac9d1a769544c040cfc3221635140c400
Jane Kamata <38163243+janekamata@users.noreply.github.com>
2025-10-17 22:39:37 -0400

    Setting up structures for dashboard usage (#174)
    * Recharts install
    
    * Added year filter action to store; updated filters to clear when switching between grants and dashboard; set up basic chart and checkboxes but fixing sync
    
    * Fixed filtering by changing processGrantData
    
    * Updating border sizes
    
    * Calendar date selected format
    
    * Sample grant calculations
    
    * Updating filter checkboxes to persist on refresh
    
    * Calculation functions for grant metrics and tests
    
    * Attempting to fix build errors

--- frontend/src/main-page/dashboard/Dashboard.tsx
+++ frontend/src/main-page/dashboard/Dashboard.tsx
@@ -1,11 +1,27 @@
-import "./styles/Dashboard.css";
 
-function Dashboard() {
+import DateFilter from "./DateFilter";
+import "./styles/Dashboard.css";
+import { observer } from "mobx-react-lite";
+import SampleChart from "./Charts/SampleChart";
+import { useEffect } from "react";
+import { updateYearFilter, updateFilter, updateEndDateFilter, updateStartDateFilter } from "../../external/bcanSatchel/actions";
+
+const Dashboard = observer(() => {
+
+  // reset filters on initial render
+    useEffect(() => {
+              updateYearFilter(null);
+              updateFilter(null);
+              updateEndDateFilter(null);
+              updateStartDateFilter(null);
+          }, []);
 
   return (
-    <div className="dashboard-page">
+    <div className="dashboard-page px-12 py-4">
+      <DateFilter />
+      <SampleChart/>
     </div>
   );
-}
+})
 
 export default Dashboard;
