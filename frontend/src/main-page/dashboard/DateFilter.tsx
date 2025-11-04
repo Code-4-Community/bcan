36f8d98725064c4dfc5092d6eff81701944dd6c7
Jane Kamata <janekamata8@gmail.com>
2025-11-03 21:17:17 -0500

    Updating checkbox styling

--- frontend/src/main-page/dashboard/DateFilter.tsx
+++ frontend/src/main-page/dashboard/DateFilter.tsx
@@ -74,7 +74,7 @@ const DateFilter: React.FC = observer(() => {
               <div className="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
                 <input
                   type="checkbox"
-                  className="w-4 h-4 rounded-sm accent-dark-orange bg-pale-orange"
+                  className="w-4 h-4 rounded-sm accent-dark-orange bg-[#FFF1EB]"
                   id={year.toString()}
                   value={year}
                   checked={selectedYears.includes(year)}
