c8ad54e60a3dea5f18bf4790c00f8a0d6d95364e
Aaron Ashby <101434393+aaronashby@users.noreply.github.com>
2025-12-02 17:19:56 -0500

    Fixed button padding and added icons

--- frontend/src/main-page/users/ApprovedUserCard.tsx
+++ frontend/src/main-page/users/ApprovedUserCard.tsx
@@ -2,6 +2,8 @@ import UserPositionCard from "./UserPositionCard";
 import { Button, Menu } from "@chakra-ui/react";
 import { FaEllipsisVertical } from "react-icons/fa6";
 import { UserStatus } from "../../../../middle-layer/types/UserStatus";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface ApprovedUserCardProps {
   name: string;
@@ -13,12 +15,12 @@ interface ApprovedUserCardProps {
 const ApprovedUserCard = ({ name, email, position }: ApprovedUserCardProps) => {
   const handleClickChangePosition = () => {
     // Open modal to confirm position change
-  }
+  };
 
   const handleClickDeleteUser = () => {
     // Open modal to confirm user deletion
-  }
-  
+  };
+
   return (
     <div className="bg-white text-lg border rounded-md m-6 p-6 flex justify-around items-center">
       <p className="font-semibold w-[140px] text-left">{name}</p>
@@ -38,19 +40,20 @@ const ApprovedUserCard = ({ name, email, position }: ApprovedUserCardProps) => {
             <Menu.Content>
               <Button
                 px={4}
-                py={2}
-                className="focus:outline-none block w-full bg-[#D3D3D3] border-[#666666] text-[#666666] mb-1"
+                className="text-sm focus:outline-none block w-full bg-[#D3D3D3] border-[#666666] text-[#666666] mb-1"
                 onClick={handleClickChangePosition}
               >
                 Change to {position === UserStatus.Admin ? "employee" : "admin"}
+                {"  "}
+                <FontAwesomeIcon icon={faPencil} />
               </Button>
               <Button
                 px={4}
-                py={2}
-                className="focus:outline-none block w-full bg-[#FFDFDF] border-[#D33221] text-[#D33221]"
+                className="text-sm focus:outline-none block w-full bg-[#FFDFDF] border-[#D33221] text-[#D33221]"
                 onClick={handleClickDeleteUser}
               >
-                Delete User
+                Delete user{"  "}
+                <FontAwesomeIcon icon={faTrash} />
               </Button>
             </Menu.Content>
           </Menu.Positioner>
