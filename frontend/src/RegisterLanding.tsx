5de07000ce6fa3368c8227fa6e4bbe0e770eb343
lyannne <xu.ly@northeastern.edu>
2026-02-04 23:45:52 -0500

    register landing fixed percentage widths

--- frontend/src/RegisterLanding.tsx
+++ frontend/src/RegisterLanding.tsx
@@ -9,8 +9,8 @@ import { useAuthContext } from "./context/auth/authContext";
 const RegisterLanding = () => {
   const {logout} = useAuthContext();
   return (
-    <div className="bg-white grid grid-cols-2 position-relative w-screen h-screen m-0 p-0 overflow-hidden flex justify-center items-start text-start">
-      <div className="w-[35%] h-full flex flex-col justify-center items-center pb-32">
+    <div className="bg-white grid grid-cols-[35%_65%] position-relative w-screen h-screen m-0 p-0 overflow-hidden flex justify-center items-start text-start">
+      <div className="h-full flex flex-col justify-center items-center pb-32">
         <div className="w-full h-[70%] bg-medium-orange rounded-r-4xl flex flex-col justify-center items-center">
           <img
             className="w-[90%] h-[90%] object-contain p-20 ml-10"
@@ -19,7 +19,7 @@ const RegisterLanding = () => {
           />
         </div>
       </div>
-      <div className="w-[65%] h-full py-20 px-24 flex flex-col justify-center items-start mb-32">
+      <div className="h-full py-20 px-24 flex flex-col justify-center items-start mb-32">
         <div className="mb-24">
           <h1 className="text-[40px] pb-8 font-bold">
             Account registration successful!
@@ -29,6 +29,7 @@ const RegisterLanding = () => {
             our team. You'll receive an email notification once your account has
             been approved. Please try logging in after receiving approval.
           </h2>
+          <br />
           <Link to="/login">
                     <button
                       style={{
