74f29c67b0046fdbf204f8461d361d9875b95522
Lyanne Xu <xu.ly@northeastern.edu>
2026-02-15 16:35:43 -0500

    Implement main view sidebar (#302)
    * placeholder cash flow page
    
    * basic starting point for side nav bar in place, put notifications in upper right corner of grant page
    
    * refined nav bar
    
    * deleted unused account components, finalized side bar
    
    * placeholder cash flow page
    
    * basic starting point for side nav bar in place, put notifications in upper right corner of grant page
    
    * refined nav bar
    
    * deleted unused account components, finalized side bar
    
    * abstracted nav tab component
    
    * updated users icon
    
    * deleted unused import
    
    * resolved copilot comments
    
    * resolved jane's comments, took branding info for nav bar out of component, fixed size of pill-shaped highlight, edited types for nav bar
    
    * deleted import
    
    * fixed import
    
    * fixed sign out button, moved nav bar branding to translations file
    
    ---------
    
    Co-authored-by: Jane Kamata <38163243+janekamata@users.noreply.github.com>

--- frontend/src/main-page/MainPage.tsx
+++ frontend/src/main-page/MainPage.tsx
@@ -1,28 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./dashboard/Dashboard";
 import GrantPage from "./grants/GrantPage";
-import Header from "./header/Header";
+import NavBar from "./navbar/NavBar";
 import Users from "./users/Users";
 import RestrictedPage from "./restricted/RestrictedPage";
+import CashFlowPage from "./cash-flow/CashFlowPage";
 import Settings from "./settings/Settings";
 
 
 function MainPage() {
-  
 
   return (
-    <div className="w-full">
-      <Header />
+    <div className="w-full h-screen flex flex-row">
+      <NavBar/>
+      <div className="flex-1 overflow-auto">
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/all-grants" element={<GrantPage showOnlyMyGrants={false} />} />
         <Route path="/my-grants" element={<GrantPage showOnlyMyGrants={true} />} /> 
         <Route path="/users" element={<Users />} />
         <Route path="/restricted" element={<RestrictedPage />} />
+        <Route path="/cash-flow" element={<CashFlowPage />} />
         <Route path="/settings" element={<Settings />} />
         {/* fallback route */}
         <Route path="*" element={<GrantPage />} />
       </Routes>
+      </div>
     </div>
   );
 }
