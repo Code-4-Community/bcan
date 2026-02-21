d46aaace9068d99e244616459bac44b6b5487869
Jane Kamata <janekamata8@gmail.com>
2026-02-21 00:39:41 -0500

    Border size

--- frontend/src/sign-up/PasswordField.tsx
+++ frontend/src/sign-up/PasswordField.tsx
@@ -77,7 +77,7 @@ export default function PasswordField({
         <input
           id={id}
           type={visible ? "text" : "password"}
-          className={`block w-full rounded-md border bg-white py-2.5 pl-4 pr-10 text-base placeholder:text-grey-500 ${
+          className={`block w-full rounded-md border-2 bg-white py-2.5 pl-4 pr-10 text-base placeholder:text-grey-500 ${
             error ? "border-red" : "border-grey-500"
           }`}
           {...inputProps}
