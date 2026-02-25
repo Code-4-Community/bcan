184921088ba16fb1715953fad0a2b8eea86fef83
Jane Kamata <janekamata8@gmail.com>
2026-02-25 01:18:43 -0500

    Button active border

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -19,7 +19,7 @@ export default function Button({ text, onClick, className, logo, logoPosition, d
   return (
     <button onClick={onClick} disabled={disabled || false} type={type || "button"}
     className={`
-        px-4 py-2 rounded-3xl font-medium text-black border-2 active:bg-primary-900 active:text-white
+        px-4 py-2 rounded-3xl font-medium text-black border-2 active:bg-primary-900 active:border-primary-900 active:text-white
         flex items-center justify-center 
         ${className}
       `}
