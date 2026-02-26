f75f44858b6a7407d14b0149a41f9d3fb6877f90
Jane Kamata <janekamata8@gmail.com>
2026-02-25 23:54:58 -0500

    Adding inactive restricted page vs not admin

--- frontend/src/main-page/restricted/RestrictedPage.tsx
+++ frontend/src/main-page/restricted/RestrictedPage.tsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 import { useAuthContext } from "../../context/auth/authContext";
 import Button from "../../components/Button";
 import BrandingPanel from "../../components/BrandingPanel";
+import { UserStatus } from "../../../../middle-layer/types/UserStatus";
+
 
 function RestrictedPage() {
-  const { logout } = useAuthContext();
+  const { logout, user } = useAuthContext();
   return (
     <div className="bg-white grid grid-cols-[75%_25%] lg:grid-cols-[50%_50%] relative w-screen h-screen m-0 p-0 overflow-hidden text-start">
       <div className="h-full  px-24 flex flex-col justify-center items-start ">
         <div className="w-full">
           <h1 className="text-4xl pb-8 font-bold">So Sorry!</h1>
           <h2 className="text-lg">
-            You don't have access to this page. Contact the admin if you think
+            {user?.position === UserStatus.Inactive ? "Your account is currently inactive or pending approval." : "You don't have access to this page."} Contact the admin if you think
             there's a mistake.
           </h2>
           <Link to="/login">
