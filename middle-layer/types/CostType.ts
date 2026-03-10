f8dcab5df177e60e886afc32e084c57118517923
prooflesben <prooflesben@gmail.com>
2026-03-10 18:20:15 -0400

    Added types for cashflow modeling

---
+++ middle-layer/types/CostType.ts
@@ -0,0 +1,12 @@
+
+
+export enum CostType{
+    Salary = "Personal Salaries",
+    Benefits = "Personal Benefits",
+    InternCompensation = "Intern Compensation",
+    MealsFood = "Meals and Food", // prob want to change this name
+    Services = "Subscriptions and Services",
+    Transportation = "Transportation",
+    RentAndSpace = "Rent and Space",
+    Other = "Other Cost"
+}
---
+++ middle-layer/types/Installment.ts
@@ -0,0 +1,4 @@
+export interface Installment {
+    amount: number;
+    date: Date;
+}
