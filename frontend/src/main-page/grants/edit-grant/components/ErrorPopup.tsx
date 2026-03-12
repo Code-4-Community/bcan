01244f525197e0abbdf2d43b5781178bdb5d7dd4
Jane Kamata <janekamata8@gmail.com>
2026-03-12 19:24:04 -0400

    Error format

--- frontend/src/main-page/grants/edit-grant/components/ErrorPopup.tsx
+++ frontend/src/main-page/grants/edit-grant/components/ErrorPopup.tsx
@@ -11,7 +11,7 @@ export default function ErrorPopup({
 }: ErrorPopupProps) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center justify-items-center z-50">
-      <div className="bg-white rounded-md px-12 py-8 max-w-md mx-4 border-2 border-grey-700 grid place-items-center">
+      <div className="bg-white rounded-md px-12 py-8 max-w-md mx-4 grid place-items-center">
         <h3 className="text-xl font-bold mb-2">Error</h3>
         <p className="mb-4">{message}</p>
         <Button
