8b6441b79aff3fa0ed198aed5c85bcfaac2a4580
prooflesben <prooflesben@gmail.com>
2026-03-14 13:19:09 -0400

    added logger to controller

--- backend/src/cashflow/cashflow.controller.ts
+++ backend/src/cashflow/cashflow.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Logger } from "@nestjs/common";
 import { ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 
@@ -6,7 +6,7 @@ import { ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
 @ApiTags('cashflow')
 @Controller('cashflow')
 export class CashflowController {
-    
+    private readonly logger = new Logger(CashflowController.name);
     @Get('hello')
     @ApiOperation({ summary: 'Hello endpoint', description: 'Returns a simple hello message' })
     @ApiResponse({ status: 200, description: 'Returns hello message' })
