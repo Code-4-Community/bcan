e3dd8d96a52d93208b4c433604a8f807edcf11f5
Jane Kamata <janekamata8@gmail.com>
2026-03-13 20:58:15 -0400

    Squashed commit of the following:
    commit d5e607aa254f50ba4e5e0f2cee32e2e904f67ab8
    Merge: 65d5876 d2ce32d
    Author: prooflesben <122566738+prooflesben@users.noreply.github.com>
    Date:   Fri Mar 13 17:04:43 2026 -0400
    
        Merge pull request #357 from Code-4-Community/353-dev---setup-cashflow-data
    
        Added types for cashflow modeling
    
    commit d2ce32ddde302fc651205576fa3cf0e23b50e2ed
    Author: prooflesben <122566738+prooflesben@users.noreply.github.com>
    Date:   Fri Mar 13 17:04:30 2026 -0400
    
        Fix typo in version declaration in amplify.yml
    
    commit f8dcab5df177e60e886afc32e084c57118517923
    Author: prooflesben <prooflesben@gmail.com>
    Date:   Tue Mar 10 18:20:15 2026 -0400
    
        Added types for cashflow modeling

---
+++ middle-layer/types/RevenueType.ts
@@ -0,0 +1,8 @@
+
+export enum RevenueType {
+    Grants = "Grants",
+    Donation = "Individual Donations",
+    Sponsorship = "Corporate Sponsorships",
+    Fundraising = "Fundraising Events",
+    Other = "Other Revenue"
+}
