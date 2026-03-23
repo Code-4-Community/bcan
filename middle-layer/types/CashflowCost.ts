094819c268a3a73b6b626d08c3be0af605774313
prooflesben <prooflesben@gmail.com>
2026-03-22 22:48:44 -0400

    added comment

--- middle-layer/types/CashflowCost.ts
+++ middle-layer/types/CashflowCost.ts
@@ -1,6 +1,7 @@
 import { TDateISO } from "../utils/date";
 import { CostType } from "./CostType";
 
+// Type defintion of a cost object of the cashflow portion of the app
 export interface CashflowCost {
     name: string;
     amount: number;
