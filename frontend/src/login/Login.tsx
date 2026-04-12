51cb218d6db94c31034ac8f76cbcf9d3e99a4c3a
Jane Kamata <janekamata8@gmail.com>
2026-04-12 15:02:59 -0400

    Remove remember me and forgot

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -37,7 +37,7 @@ const Login = observer(() => {
           <h1 className="text-3xl lg:text-4xl font-bold flex justify-start">Log in</h1>
         </div>
         <form onSubmit={handleSubmit} className="w-full">
-          <div className="grid grid-cols-1 gap-x-6 gap-y-4">
+          <div className="grid grid-cols-1 gap-x-6">
             <div className="mb-4">
               <div>
                 <InputField id="email" label="Email" type="email" required placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} error={failure} />
@@ -59,7 +59,7 @@ const Login = observer(() => {
           </div>
 
           {/* Remember Me & Forgot Password */}
-          <div className="flex items-center justify-between mb-6">
+          {/* <div className="flex items-center justify-between mb-6">
             <label className="flex items-center cursor pointer">
               <input
                 type="checkbox"
@@ -76,7 +76,7 @@ const Login = observer(() => {
             >
               Forgot Password?
             </button>
-          </div>
+          </div> */}
 
           {/* Error Message */}
           <div className="">
